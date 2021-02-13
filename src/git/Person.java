e89d434971b5b055727343ad98c4ab740098b54e
Bohdan <bohdanpodzihun@gmail.com>
2021-02-13 16:00:32 +0200

    4th commit

---
+++ src/git/Person.java
@@ -0,0 +1,10 @@
+package git;
+
+public class Person {
+    public static void main(String[] args) {
+        String surname = "Podzihun";
+        String like = "like Java";
+
+        System.out.println(surname + " " + like);
+    }
+}
