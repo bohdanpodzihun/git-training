9bf24114a4c0cf0168fac50b705900d3c17056f5
Bohdan <bohdanpodzihun@gmail.com>
2021-02-13 16:12:49 +0200

    4.1 commit

--- src/git/Main.java
+++ src/git/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
         double height = 1.93;
         System.out.println("height : " + height);
-        System.out.println("i should delete this");
+        System.out.println();
+        System.out.println("See you again");
     }
 }
